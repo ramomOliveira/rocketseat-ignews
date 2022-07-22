ae0570b3887efd69845d257988ccbd8f6af35063
Ramom Oliveira <ra-mom2000@hotmail.com>
2022-07-21 23:02:43 -0300

    listening to webhooks

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -7,8 +7,6 @@ import styles from './styles.module.scss';
 export function SignInButton() {
   const { data: session, status } = useSession();
 
-  console.log(status, session);
-
   return status === 'authenticated' ? (
     <button
       className={styles.signInButton}
--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,12 +1,40 @@
 import styles from './styles.module.scss';
+import { signIn, useSession } from 'next-auth/react';
+import { api } from '../../services/api';
+import { getStripeJs } from '../../services/stripe-js';
 
 interface SubscribeButtonProps {
   priceId: string;
 }
 
 export function SubscribeButton({ priceId }: SubscribeButtonProps) {
+  const { data: session, status } = useSession();
+
+  async function handleSubscribe() {
+    if (status === 'unauthenticated') {
+      signIn('github');
+      return;
+    }
+
+    try {
+      const response = await api.post('/subscribe');
+
+      const { sessionId } = response.data;
+
+      const stripe = await getStripeJs();
+
+      await stripe.redirectToCheckout({ sessionId: sessionId });
+    } catch (err) {
+      alert(err.message);
+    }
+  }
+
   return (
-    <button type='button' className={styles.subscribeButton}>
+    <button
+      type='button'
+      className={styles.subscribeButton}
+      onClick={handleSubscribe}
+    >
       Subscribe now
     </button>
   );
